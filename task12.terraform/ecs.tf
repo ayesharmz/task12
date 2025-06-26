997ca32e19670d597622666acfed60b4f7bb53d3
ayesharmz <119579227+ayesharmz@users.noreply.github.com>
2025-06-26 18:57:57 +0530

    Update ecs.tf

--- task12.terraform/ecs.tf
+++ task12.terraform/ecs.tf
@@ -82,6 +82,6 @@ resource "aws_ecs_service" "strapi" {
 resource "aws_s3_object" "appspec" {
   bucket = "strapi786"
   key    = "deploy-strapi.zip"
-  source = "${path.module}/deploy-strapi.zip"
-  etag   = filemd5("${path.module}/deploy-strapi.zip")
+  source = "${path.module}/../deploy-strapi.zip"
+  etag   = filemd5("${path.module}/../deploy-strapi.zip")
 }
