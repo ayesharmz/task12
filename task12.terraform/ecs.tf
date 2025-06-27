404954b6593b862afc47e89a80b877cf5bd3a42d
ayesharmz <119579227+ayesharmz@users.noreply.github.com>
2025-06-27 12:56:56 +0530

    Update ecs.tf

--- task12.terraform/ecs.tf
+++ task12.terraform/ecs.tf
@@ -2,7 +2,17 @@
 // ecs.tf
 // ----------------------------
 
+# ----------------------------
+# Variable for dynamic image tag
+# ----------------------------
+variable "image_tag" {
+  type    = string
+  default = "latest"
+}
+
+# ----------------------------
 # ECS Security Group
+# ----------------------------
 resource "aws_security_group" "ecs_service" {
   name        = "ecs-service-sg"
   description = "Allow inbound traffic to ECS service"
@@ -27,14 +37,18 @@ resource "aws_security_group" "ecs_service" {
   }
 }
 
+# ----------------------------
 # ECS Cluster
+# ----------------------------
 resource "aws_ecs_cluster" "strapi" {
   name = "strapi-cluster"
 }
 
+# ----------------------------
 # ECS Task Definition
+# ----------------------------
 resource "aws_ecs_task_definition" "strapi" {
-  family                   = "strapi-task-def"
+  family                   = "strapi-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "512"
@@ -43,7 +57,7 @@ resource "aws_ecs_task_definition" "strapi" {
 
   container_definitions = jsonencode([{
     name      = "strapi"
-    image     = "shunnualisha8980/strapi-app:latest"
+    image     = "shunnualisha8980/strapi-app:${var.image_tag}"
     essential = true
     portMappings = [
       {
@@ -54,7 +68,9 @@ resource "aws_ecs_task_definition" "strapi" {
   }])
 }
 
-# ECS Service with Load Balancer for CodeDeploy
+# ----------------------------
+# ECS Service with CodeDeploy + Load Balancer
+# ----------------------------
 resource "aws_ecs_service" "strapi" {
   name            = "strapi-service"
   cluster         = aws_ecs_cluster.strapi.id
@@ -79,6 +95,9 @@ resource "aws_ecs_service" "strapi" {
   }
 }
 
+# ----------------------------
+# S3 Upload for AppSpec File
+# ----------------------------
 resource "aws_s3_object" "appspec" {
   bucket = "strapi786"
   key    = "deploy-strapi.zip"
